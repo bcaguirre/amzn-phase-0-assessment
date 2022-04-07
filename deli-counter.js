fde7428b28db9be88cca32d3d5e0db24149d247a
bernadette <aguirre.bernadette@outlook.com>
2022-04-07 17:19:17 -0600

    Completed Assignment

--- deli-counter.js
+++ deli-counter.js
@@ -1,24 +1,54 @@
 // 1. Write your functions here
+function line(katzDeli){
+ 
+} else {
+        "The line is currently empty."
+    }
+}
+
+function takeANumber(katzDeli, newCustomer) {
+
+    console.log(ex. "Welcome, .....")
+}
+
+function nowServing(katzDeli){
+
+    console.log(ex. "Now serving...")
+} else {
+    console.log("There is nobody waiting to be served!") //if nobody in line
+}
+
+}
+
+
+
+//Review adding and removing elements from an array as well as iterating with index numbers.
+// also, many of the methods to add and remove elements from an array can also be used to add and remove elements to a string.
+
+
+//OBJECTIVE:
+//1.) Practice building methods that use iteration and controlling their return values.
+//2.) Practice manipulating arrays (adding elements, removing elements, etc.)
 
 
 // 2. Example Usage
 
-// const katzDeli = []
+ /*const katzDeli = []
 
-// takeANumber(katzDeli, "Ada") //=> Welcome, Ada. You are number 1 in line.
-// takeANumber(katzDeli, "Grace") //=> Welcome, Grace. You are number 2 in line.
-// takeANumber(katzDeli, "Kent") //=> Welcome, Kent. You are number 3 in line.
+ takeANumber(katzDeli, "Ada") //=> Welcome, Ada. You are number 1 in line.
+ takeANumber(katzDeli, "Grace") //=> Welcome, Grace. You are number 2 in line.
+ takeANumber(katzDeli, "Kent") //=> Welcome, Kent. You are number 3 in line.
 
-// line(katzDeli) //=> "The line is currently: 1. Ada 2. Grace 3. Kent"
+ line(katzDeli) //=> "The line is currently: 1. Ada 2. Grace 3. Kent"
 
-// nowServing(katzDeli) //=> "Currently serving Ada."
+ nowServing(katzDeli) //=> "Currently serving Ada."
 
-// line(katzDeli) //=> "The line is currently: 1. Grace 2. Kent"
+ line(katzDeli) //=> "The line is currently: 1. Grace 2. Kent"
 
-// takeANumber(katzDeli, "Matz") //=> Welcome, Matz. You are number 3 in line.
+ takeANumber(katzDeli, "Matz") //=> Welcome, Matz. You are number 3 in line.
 
-// line(katzDeli) //=> "The line is currently: 1. Grace 2. Kent 3. Matz"
+ line(katzDeli) //=> "The line is currently: 1. Grace 2. Kent 3. Matz"
 
-// nowServing(katzDeli) //=> "Currently serving Grace."
+ nowServing(katzDeli) //=> "Currently serving Grace."
 
-// line(katzDeli) //=> "The line is currently: 1. Kent 2. Matz"
+ line(katzDeli) //=> "The line is currently: 1. Kent 2. Matz"*/
